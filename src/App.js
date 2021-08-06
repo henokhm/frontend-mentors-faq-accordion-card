77769f1e867157e453865efaedbd023a113f81a7
Henok Hailemariam <henokhm@pop-os.localdomain>
2021-08-06 09:10:14 +0300

    Moved faq data to faqs.json

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
+import faqsData from "./faqs.json";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      How many team members can I invite? You can invite up to 2 additional
-      users on the Free plan. There is no limit on team members for the Premium
-      plan. What is the maximum file upload size? No more than 2GB. All files in
-      your account must fit your allotted storage space. How do I reset my
-      password? Click “Forgot password” from the login page or “Change password”
-      from your profile page. A reset link will be emailed to you. Can I cancel
-      my subscription? Yes! Send us a message and we’ll process your request no
-      questions asked. Do you provide additional support? Chat and email support
-      is available 24/7. Phone lines are open during normal business hours.
+      <h1>FAQ</h1>
+      {faqsData.map((item) => (
+        <div key={item.id}>
+          <div>Question: {item.question}</div>
+          <div>Answer: {item.answer}</div>
+        </div>
+      ))}
     </div>
   );
 }
